0ddf38735f750f793bd33b9c5d7cda94c89a9356
Fabian Schwahn <fabian.schwahn@gmail.com>
2021-07-07 14:03:58 +0200

    Keep git clean after spec has run

--- spec/alphabetical_schema_spec.rb
+++ spec/alphabetical_schema_spec.rb
@@ -1,11 +1,20 @@
 RSpec.describe AlphabeticalSchema do
+  let(:schema_path) { "spec/internal/db/schema.rb" }
+
+  around do |example|
+    original_schema = File.read(schema_path)
+    example.run
+    File.write(schema_path, original_schema)
+  end
+
   it "creates an alphabetically sorted schema" do
-    expected_lines = File.readlines("spec/expected_schema/schema_6.1.rb")
+    expected_lines = File.readlines("#{__dir__}/expected_schema/schema_6.1.rb")
 
-    puts `cd spec/internal && rake db:migrate`
+    Dir.chdir("#{__dir__}/internal") do
+      system("rake db:migrate")
+    end
 
-    generated_lines = File.readlines("spec/internal/db/schema.rb")
-
+    generated_lines = File.readlines(schema_path)
     generated_lines.zip(expected_lines).each do |generated, expected|
       expect(generated).to eq(expected)
     end
