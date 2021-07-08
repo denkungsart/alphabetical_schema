2dab70bb63862bd8da05c864d80b41d1e420a71e
Fabian Schwahn <fabian.schwahn@gmail.com>
2021-07-08 12:37:42 +0200

    Generally patch SchemaDumper to sort
    It’s actually not necessary to limit this to the rake task, instead we generally patch ActiveRecord::SchemaDumper, which is much simpler.

--- lib/alphabetical_schema.rb
+++ lib/alphabetical_schema.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
-require "alphabetical_schema/railtie"
-require "alphabetical_schema/schema_dumper"
 require "alphabetical_schema/version"
+
+module AlphabeticalSchema
+  class SortedConnection < SimpleDelegator
+    def columns(table_name)
+      __getobj__.columns(table_name).sort_by(&:name)
+    end
+  end
+
+  module SchemaDumperPatch
+    def initialize(*)
+      super
+      @connection = ::AlphabeticalSchema::SortedConnection.new(@connection)
+    end
+  end
+end
+
+require "active_record/schema_dumper"
+ActiveRecord::SchemaDumper.prepend(::AlphabeticalSchema::SchemaDumperPatch)
